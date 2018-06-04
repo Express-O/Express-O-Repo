f86c9e1012cc08c9769489d9d91b6df9ee30c595
Molly Seeley <mseeley89@gmail.com>
2018-06-04 19:43:21 -0400

    initial commit

---
+++ server/db/models/product.js
@@ -0,0 +1,33 @@
+const db = require('../db/db')
+const Sequelize = require('sequelize')
+
+const Product = db.defint('product', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    defaultValue: 0
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  photo: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/No_image_available_600_x_450.svg/640px-No_image_available_600_x_450.svg.png'
+  },
+  category: {
+    type: Sequelize.ENUM,
+    values: ['category 1', 'category 2']
+  }
+})
+
+module.exports = Product
