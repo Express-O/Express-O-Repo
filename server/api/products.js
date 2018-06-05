49e6d310af4c1118a12862ee629dd6610eb5d370
Jenny Gao <jennygao1023@gmail.com>
2018-06-05 18:32:20 -0400

    single component working

--- server/api/products.js
+++ server/api/products.js
@@ -11,4 +11,11 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    res.json(product);
+  } catch (error) { next(error) }
+})
+
 module.exports = router
