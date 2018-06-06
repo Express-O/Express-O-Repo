2775d5d0ac4273a68fd68943511e1a7b104c5d5b
Jenny Gao <jennygao1023@gmail.com>
2018-06-05 22:57:49 -0400

    reorganized AllProducts component

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,49 +1,38 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
-import { fetchProducts } from '../store'
+import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
+import { fetchProducts } from '../store/index'
 
 //need to add a ternary in return for if it is admin show add button
 class AllProducts extends Component {
-  constructor() {
-    super()
-    this.state = {
-      category: []
-    }
-  }
-
   componentDidMount() {
     this.props.fetchProducts();
   }
 
   render() {
-    console.log('LOCAL STATE', this.state)
-    // console.log('ALL PRODUCTS', this.props.allProducts)
+    const { allProducts, allDrinks, allSwag, location } = this.props;
     let arr;
-    console.log('location=======>', this.props.location)
-    if (this.props.location.pathname === "/product/coffee") {
-      console.log('HEEREE!!')
-      arr = this.props.allDrinks
-    } else if (this.props.location.pathname === "/product/swag") {
-      arr = this.props.allSwag
+    if (location.pathname === '/product/coffee'){
+      arr = allDrinks;
+    } else if (location.pathname === '/product/swag'){
+      arr = allSwag;
     } else {
-      arr = this.props.allProducts
+      arr = allProducts;
     }
-
-    console.log('ARRAY', arr)
     return (
       <div>
         <ul>
           {
             arr.map(product => {
-              return (
-                <li key={product.id}>
-                  <img src={product.photo} />
-                  <Link to={`/products/${product.id}`}><p>{product.title}</p></Link>
-                  <p>{product.price}</p>
-                </li>
-              )
-            })
+            return (
+              <div key={product.id}>
+                <img src ={product.photo} />
+                <Link to={`/products/${product.id}`}>
+                  <p>{product.title}</p>
+                </Link>
+                <p>{product.price}</p>
+              </div>
+            )})
           }
         </ul>
       </div>
@@ -51,11 +40,12 @@ class AllProducts extends Component {
   }
 }
 const mapStateToProps = state => {
+  const allProducts = state.allProducts
   return (
     {
-      allProducts: state.allProducts,
-      allDrinks: state.allProducts.filter(drink => drink.category === 'drink'),
-      allSwag: state.allProducts.filter(swag => swag.category === 'swag')
+      allProducts: allProducts,
+      allDrinks: allProducts.filter(drink => drink.category === 'drink'),
+      allSwag: allProducts.filter(swag => swag.category === 'swag')
     }
   )
 }
