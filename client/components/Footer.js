640eebc8c2d1a2f1877781e1615d3c3d070c27c4
Molly Seeley <mseeley89@gmail.com>
2018-06-05 22:42:35 -0400

    dummy contact info

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     <div>
       <h1>Contact Info</h1>
       <h2>Email: expressO@exressO.com</h2>
+      <h2>Instagram: express-O</h2>
       <h2>Twitter: @expressO</h2>
       <h2>Phone: 555-555-5555</h2>
     </div>
