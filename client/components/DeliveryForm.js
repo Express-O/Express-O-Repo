8c7975c2d9a162cbfba33842171aab04cf1f38b4
Crystal Mora <ChibiMora@users.noreply.github.com>
2018-06-08 15:03:21 -0400

    created a guest delivery form that may not be used as a guest they will be redirected but rather this form may be used as a user edit form

---
+++ client/components/DeliveryForm.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+//For guests who aren't logged in/don't have an account to 
+
+export default class DeliveryForm extends Component{
+  constructor(){
+    super()
+    this.state = {
+      firstName: '',
+      lastName: '',
+      telephone: '',
+      email: '',
+      streetName: '',
+      apt: '',
+      city: '',
+      state: '',
+      zip: '',
+      country: ''
+    }
+    this.clickHandler = this.clickHandler.bind(this)
+    this.submitHandler = this.submitHandler.bind(this)
+  }
+
+  clickHandler(evt){
+    this.setState({[evt.target.name]: evt.target.value})
+  }
+
+  submitHandler(evt){
+    evt.preventDefault()
+    //
+    this.setState({
+      firstName: '',
+      lastName: '',
+      telephone: '',
+      email: '',
+      streetName: '',
+      apt: '',
+      city: '',
+      state: '',
+      zip: '',
+      country: ''})
+  }
+  render(){
+    return (
+      <form>
+        <label htmlFor="firstName"><small>First Name:</small></label>
+        <input name="firstName" type="text" />
+        <label htmlFor="lastName"><small>Last Name:</small></label>
+        <input name="lastName" type="text" />
+        <label htmlFor="telephone"><small>Telephone:</small></label>
+        <input name="telephone" type="text" />
+        <label htmlFor="email"><small>Email</small></label>
+        <input name="email" type="text" />
+        <label htmlFor="streetName"><small>Street Adress:</small></label>
+        <input name="streetName" type="text" />
+        <label htmlFor="apt"><small>Apt:</small></label>
+        <input name="apt" type="text" />
+        <label htmlFor="city"><small>City:</small></label>
+        <input name="city" type="text" />
+        <label htmlFor="state"><small>State:</small></label>
+        <input name="state" type="text" />
+        <label htmlFor="zip"><small>Zip:</small></label>
+        <input name="zip" type="text" />
+        <label htmlFor="country"><small>Country:</small></label>
+        <input name="country" type="text" />
+      </form>
+    )
+  }
+}
+
