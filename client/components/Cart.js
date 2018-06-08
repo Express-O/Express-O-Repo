ec1853c69ec0b77a6a10472cc86156dbb56c8eb1
Joyce Chow <joycecyoj01@gmail.com>
2018-06-08 01:17:10 -0400

    completed empty cart feature

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
-import { addCart, fetchCart, removeProduct } from '../store/index';
+import { addCart, fetchCart, removeProduct, emptyCart } from '../store/index';
 import ProductCard from './ProductCard'
 
 class Cart extends Component {
+  constructor() {
+    super()
+  }
 
   componentDidMount() {
     this.props.fetchCart()
@@ -12,17 +15,21 @@ class Cart extends Component {
 
   render() {
     const { cart } = this.props;
+    
     return (
       <div>
         <h1>Shopping Cart</h1>
         {
-          cart[0] ? null : <p>No Items in Cart!</p>
+          cart[0] ? null : <p>Your shopping cart is empty!</p>
         }
         <ul>
           {
             cart.map(product => (<ProductCard key={product.id} product={product} removeProduct={this.props.removeProduct} cartProductCount={cart.length} />))
           }
         </ul>
+        {
+          cart[0] ?  <button type='button' onClick={() => this.props.emptyCart()}>Empty your cart</button> : null
+        }
       </div>
     )
   }
@@ -38,7 +45,8 @@ const mapDispatch = dispatch => {
   return {
     addCart: (product) => dispatch(addCart(product)),
     fetchCart: () => dispatch(fetchCart()),
-    removeProduct: (productId) => dispatch(removeProduct(productId))
+    removeProduct: (productId) => dispatch(removeProduct(productId)),
+    emptyCart: () => dispatch(emptyCart())
   }
 }
 
--- server/api/cart.js
+++ server/api/cart.js
@@ -10,21 +10,6 @@ router.get('/', (req, res, next) => {
   }
 })
 
-// POST new product
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const newProduct = await Product.create({
-//       title: req.body.title,
-//       description: req.body.description,
-//       price: req.body.price,
-//       inventory: req.body.inventory,
-//       photo: req.body.photo,
-//       category: req.body.category
-//     })
-//     res.status(201).json(newProduct)
-//   } catch (error) { next(error) }
-// })
-
 // api/cart
 // PUT update cart with product
 router.put('/', (req, res, next) => {
@@ -38,22 +23,28 @@ router.put('/', (req, res, next) => {
   }
 })
 
+// api/cart
+// Empty Cart
+router.delete('/', (req, res, next) => {
+  try {
+    req.session.cart = []
+    res.json(req.session.cart)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
 // api/cart/productId
 // DELETE (Remove) product from cart
 router.delete('/:productId', (req, res, next) => {
   try {
     const productToRemoveId = +req.params.productId
-    console.log(productToRemoveId)
     const cartCopy = req.session.cart.slice()
     const filteredCart = cartCopy.filter(product => {
-      console.log(product.id, productToRemoveId)
       return product.id !== productToRemoveId
     })
-
-    console.log('productToRemoveId', req.params.productId)
-    console.log('equal', cartCopy.length == filteredCart.length)
-    console.log('Filtered Cart=====', filteredCart)
-
+    
     req.session.cart = filteredCart
     res.status(200).json(filteredCart)
   } catch (error) { next(error) }
