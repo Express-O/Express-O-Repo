f779d745700218270d697585cf28869f60955b03
Joyce Chow <joycecyoj01@gmail.com>
2018-06-08 18:16:54 -0400

    shopping cart adds everything

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -16,6 +16,39 @@ class Cart extends Component {
   render() {
     const { cart } = this.props;
     
+    let cartWithQty = cart.reduce((cartHashTbl, product) => {
+      // console.log('product from hashtbl', product)
+      let title = product.title
+      if (!cartHashTbl[title]) {
+        product.quantity = 1
+        cartHashTbl[title]= product
+        // console.log('inside if block')
+        // console.log('cartHashTbl', cartHashTbl)
+      } else {
+        // console.log('made it to else block - cartHashTbl', cartHashTbl)
+        cartHashTbl[title].quantity++
+      }
+      return cartHashTbl
+    }, {})
+
+      console.log('CART W QTY', cartWithQty)
+
+
+      let helperFunc = (obj) => {
+        let cartWithQtyArr = []
+        for (var title in cartWithQty) {
+          let product = cartWithQty[title]
+          console.log('product', product)
+          cartWithQtyArr.push(product)
+        }
+        return cartWithQtyArr
+      }
+      let cartWithQtyArr = helperFunc(cartWithQty)
+
+
+
+
+
     return (
       <div>
         <h1>Shopping Cart</h1>
@@ -25,21 +58,25 @@ class Cart extends Component {
           <div>
           <ul>
             {
-            cart.map(product => {
-              console.log("JENNY LOOK HERE===============================>", cart)
+            cartWithQtyArr.map(product => {
+              // console.log("JENNY LOOK HERE===============================>", cart)
+              let cartId = Math.random()
               return (
               <ProductCard
-                key={product.id}
+                key={cartId}
                 product={product}
                 removeProduct={this.props.removeProduct}
                 cartProductCount={cart.length}
+                quantity={product.quantity}
               />
               )
             })
+
+
           }
           <button type='button' onClick={() => this.props.emptyCart()}>Empty your cart</button>
           </ul>
-          <span className="badge">{cart.length}</span>
+          {/* <span className="badge">{cart.length}</span> */}
           </div>
         : <p>Your shopping cart is empty!</p>
         }
