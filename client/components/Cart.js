a80f6067df5a5307b96c5d1de4679a9bed98815d
Joyce Chow <joycecyoj01@gmail.com>
2018-06-08 14:42:06 -0400

    empty cart feature

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-<<<<<<< HEAD
 import { Link, withRouter } from 'react-router-dom'
 import { addCart, fetchCart, removeProduct, emptyCart } from '../store/index';
-=======
-import { withRouter } from 'react-router-dom'
-import { addCart, fetchCart, removeProduct } from '../store/index';
->>>>>>> master
 import ProductCard from './ProductCard'
 
 class Cart extends Component {
@@ -25,18 +20,6 @@ class Cart extends Component {
       <div>
         <h1>Shopping Cart</h1>
         {
-<<<<<<< HEAD
-          cart[0] ? null : <p>Your shopping cart is empty!</p>
-        }
-        <ul>
-          {
-            cart.map(product => (<ProductCard key={product.id} product={product} removeProduct={this.props.removeProduct} cartProductCount={cart.length} />))
-          }
-        </ul>
-        {
-          cart[0] ?  <button type='button' onClick={() => this.props.emptyCart()}>Empty your cart</button> : null
-        }
-=======
           cart[0] 
           ? 
           <div>
@@ -53,13 +36,13 @@ class Cart extends Component {
               />
               )
             })
-            }
+          }
+          <button type='button' onClick={() => this.props.emptyCart()}>Empty your cart</button>
           </ul>
           <span className="badge">{cart.length}</span>
           </div>
-        : <p>No Items in Cart!</p>
+        : <p>Your shopping cart is empty!</p>
         }
->>>>>>> master
       </div>
     )
   }
