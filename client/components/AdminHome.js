c4c9fbe50ead0561a3cc81418f555bf697ef2397
Crystal Mora <ChibiMora@users.noreply.github.com>
2018-06-12 17:53:19 -0400

    admin home page has containers to hold the dashboard buttons and an img

--- client/components/AdminHome.js
+++ client/components/AdminHome.js
@@ -7,21 +7,28 @@ const AdminHome = (props) => {
   return (
     <div>
       <h3>Welcome, {props.user.firstName} {props.user.lastName}</h3>
-      <Link to="/admin/inventory">
-        <button type="button">VIEW PRODUCT INVENTORY</button>
-      </Link>
-      <Link to="/admin/addproduct">
-        <button type="button">ADD NEW PRODUCT</button>
-      </Link>
-      <Link to="/admin/useraccounts">
-        <button type="button">VIEW USER ACCOUNTS</button>
-      </Link>
-      <Link to="/signup">
-        <button type="button">ADD NEW USER ACCOUNT</button>
-      </Link>
-      <Link to="/admin/allorders">
-        <button type="button">VIEW ALL ORDERS</button>
-      </Link>
+      <div className="admin-home-container">
+        <div className="home-desc" >
+          <Link to="/admin/inventory">
+            <button type="button">VIEW PRODUCT INVENTORY</button>
+          </Link>
+          <Link to="/admin/addproduct">
+            <button type="button">ADD NEW PRODUCT</button>
+          </Link>
+          <Link to="/admin/useraccounts">
+            <button type="button">VIEW USER ACCOUNTS</button>
+          </Link>
+          <Link to="/signup">
+            <button type="button">ADD NEW USER ACCOUNT</button>
+          </Link>
+          <Link to="/admin/allorders">
+            <button type="button">VIEW ALL ORDERS</button>
+          </Link>
+        </div>
+      <div className="userhomepageimg">
+        <img src="https://www.healthline.com/hlcmsresource/images/AN_images/espresso-ground-coffee-beans-1296x728.jpg" />
+      </div>
+      </div>
     </div >
   )
 }
